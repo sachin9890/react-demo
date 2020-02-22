acfb310e72c6df872282fece54885bacc70224ed
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-22 17:35:02 +0000

    Deploy sachin9890/react-demo to github.com/sachin9890/react-demo.git:gh-pages

---
+++ precache-manifest.9db19347ce72bffd35aef11bba504e21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac4c05edfd50b8e894e8b5c65e9cb63",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4cdf84a7b8a80d10a06d",
+    "url": "./static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a5f6c5eb93695e755703",
+    "url": "./static/css/main.eeac99db.chunk.css"
+  },
+  {
+    "revision": "4cdf84a7b8a80d10a06d",
+    "url": "./static/js/2.977e235a.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "./static/js/2.977e235a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f6c5eb93695e755703",
+    "url": "./static/js/main.966719bf.chunk.js"
+  },
+  {
+    "revision": "8bfd81403f75057bc415",
+    "url": "./static/js/runtime-main.7fddb8b1.js"
+  },
+  {
+    "revision": "b21122d150564b49c0c9f7ef5813d6b7",
+    "url": "./static/media/background7.b21122d1.jpg"
+  }
+]);
